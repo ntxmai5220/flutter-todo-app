b81521bfa0869deebe124ece7e4b2dce447d698f
Xuan Mai <xuanmai.k18@gmail.com>
2021-11-17 22:13:05 +0700

    basic UI

---
+++ lib/values/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static List<Color> custom = [
+    Colors.yellow,
+    Colors.orange,
+    Colors.red,
+    Colors.lime,
+    Colors.lightGreen,
+    Colors.blue,
+    Colors.purple,
+  ];
+}
---
+++ lib/values/app_styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static TextStyle normal = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  static TextStyle title = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+  static TextStyle detailTitle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+}
